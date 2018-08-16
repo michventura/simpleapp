73df78e404d54ace557ca298ba413ba434cfd6b3
Mich <michcodes@hotmail.com>
2018-08-16 18:24:35 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="main">
+        <section className="header">
+          <h1>This is the header area</h1>
+        </section>
+        <section className="body">
+          <h1>This is the content area</h1>
+        </section>
+        <section className="footer">
+          <h1>This is the footer area</h1>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default App;
